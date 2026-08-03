7e6a3bddb755b9681a84db90872df6a3bd4e6104
funzzzz <roqa26121990@gmail.com>
2026-08-03 23:10:19 +0300

    Migrate to maintained FFmpegKit library 8.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("com.google.mlkit:text-recognition:16.0.1")
     
     // FFmpeg Kit (جاهز، لا يحتاج NDK)
-    implementation("com.arthenica:ffmpeg-kit-full:5.1.LTS")
+    implementation("dev.ffmpegkit-maintained:ffmpeg-kit-full:8.1.7")
     
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
